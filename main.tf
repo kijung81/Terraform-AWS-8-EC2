78bbf2124d60e83c854f47af407091fd24434992
MZC01-MYNECK <mzc01-myneck@MZC01-MYNECK.local>
2023-10-05 16:38:30 +0900

    6 sg add

--- main.tf
+++ main.tf
@@ -69,7 +69,7 @@ resource "aws_security_group" "web" {
     from_port        = 443
     to_port          = 443
     protocol         = "tcp"
-    cidr_blocks      = [var.vpc_cidr]
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   ingress {
@@ -78,8 +78,15 @@ resource "aws_security_group" "web" {
     to_port          = 80
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
-  }  
+  }
 
+  ingress {
+    description      = "ssh"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }  
   egress {
     from_port        = 0
     to_port          = 0
