61a97e576bcad0b43cb1dc4bd5431bd33a43152e
MZC01-MYNECK <mzc01-myneck@MZC01-MYNECK.local>
2023-10-07 16:49:51 +0900

    vpc cidr prefix change

--- variables.tf
+++ variables.tf
@@ -1,17 +1,17 @@
 variable vpc_cidr {
-  default = "192.168.0.0/16"
+  default = "10.30.0.0/16"
 }
 
 variable pub_sbn_a {
-  default = "192.168.1.0/24"
+  default = "10.30.1.0/24"
 }
 
 variable pub_sbn_b {
-  default = "192.168.2.0/24"
+  default = "10.30.2.0/24"
 }
 
 variable prefix {
-  default = "Terraform-PoC"
+  default = "Terraform-PoC-8-ec2"
 }
 
 variable ec2_key{
